d4e993688b0c1be1ff204ede4119f1e098f7e31c
Anatoly <krest.66@mail.ru>
2020-11-15 19:04:19 +0300

    add multiple port

--- __main__.py
+++ __main__.py
@@ -1,18 +1,5 @@
 from GUI import GUI
-from backend import ModuleManipulator
 
 
 if __name__ == '__main__':
-	'''
-	try:
-		settings = GUI().start_gui()
-	except Exception as e:
-		print('GUI ERROR: ', e)
-
-	try:
-		ModuleManipulator(settings).start()
-	except Exception as e:
-		print('BACKEND ERROR: ', e)
-	'''
 	settings = GUI().start_gui()
-	ModuleManipulator(settings).start()
--- backend/modules/dash/Dashboard.py
+++ backend/modules/dash/Dashboard.py
@@ -13,12 +13,13 @@ from logs.logger import debug
 
 class Dashboard(ABC):
 	'''Dashboard Interface'''
-
+	port = 8000
 	def __init__(self):
 		external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 		external_scripts = ['https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML']
 		self.app = dash.Dash(
 			server=True, external_stylesheets=external_stylesheets, external_scripts=external_scripts)
+		Dashboard.port += 1
 
 	@debug
 	@abstractmethod
@@ -28,5 +29,7 @@ class Dashboard(ABC):
 	@debug
 	def start(self, debug=False):
 		self.app.layout = self._generate_layout()
-		webbrowser.open("http://127.0.0.1:8050/")
-		self.app.run_server(debug=debug)
+
+		port = Dashboard.port
+		webbrowser.open(f"http://127.0.0.1:{port}/")
+		self.app.run_server(debug=debug, port=port)
