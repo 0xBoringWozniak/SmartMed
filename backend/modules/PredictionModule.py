df3cfc3411030da88bb02c8aed051c76b32bcbc7
Anatoly <krest.66@mail.ru>
2020-11-22 22:09:04 +0300

    added regression models
    1. ModelCreator
    2. ModelInterface
    3. LinReg, LogReg, PolyReg interfaces

--- backend/modules/PredictionModule.py
+++ backend/modules/PredictionModule.py
@@ -1,8 +1,22 @@
 from typing import Dict
 
+import numpy as np
+
+
 from .ModuleInterface import Module
 from .dash import PredictionDashboard
 
+from .ModelManipulator import ModelManipulator
+
 
 class PredictionModule(Module, PredictionDashboard):
-	pass
+	
+	def _prepare_dashboard_settings(self):
+
+		# tmp
+        x = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+        y = np.dot(x, np.array([1, 2])) + 3
+
+        self.model = ModelManipulator(model_type=settings['model'], x, y).create()
+
+        super().__init__(settings)
